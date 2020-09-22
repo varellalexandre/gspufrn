46e44e7531fa610ac70a94d3e522709e2fa9fbed
varellalexandre <varellalexandre@gmail.com>
2020-09-22 08:00:32 -0300

    Atualizando chips

--- lib/design/field/crud_dialog.dart
+++ lib/design/field/crud_dialog.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gspufrn/models/job.dart';
 import 'package:gspufrn/design/field/editable.dart';
 import 'package:gspufrn/design/field/color_theme.dart' as theme;
+import 'package:chips_choice/chips_choice.dart';
 
 
 enum crudDialogAction{
@@ -22,91 +23,77 @@ class crudDialog extends StatelessWidget{
 
 	@override
 	Widget build(BuildContext context){
-		List<Row> activities = List();
-		ScrollController scroll_controller = ScrollController(
-			initialScrollOffset:0.0,
-			keepScrollOffset:true,
-			debugLabel:'Scroll',
-		);
-		model.atividades.keys.forEach(
-			(element)=>activities.add(
-				Row(
-					children:[
-						Expanded(
-							child:Checkbox(
-								value:true,
-							)
-						)
-					]
-
-				)
-			)
-		);
 		if(action == crudDialogAction.add){
+			List<ChipsChoiceOption> opts = List();
+			List<String> marked = List();
+			model.atividades.keys.forEach((value){
+				opts.add(
+					ChipsChoiceOption(
+						value:value,
+						label:model.atividades[value].nome,
+					)
+				);
+			});
 			return AlertDialog(
 				title:Text('Atividade'),
 				titlePadding:EdgeInsets.all(10.0),
 				content:Container(
-					child:ListView(
+					child:Column(
 						children:[
-							Column(
-								children:[
-									Expanded(
-										flex:1,
-										child:Align(
-											alignment:Alignment.centerLeft,
-											child:FittedBox(
-												fit:BoxFit.scaleDown,
-												child:Text('Nome :')
-											)
-										)
-									),
-									Expanded(
-										flex:3,
-										child:editable(
-											beingEdited:false,
-											controller:nome_controller,
-										)
-									),
-									Expanded(
-										flex:1,
-										child:Align(
-											alignment:Alignment.centerLeft,
-											child:FittedBox(
-												fit:BoxFit.scaleDown,
-												child:Text('Tempo :')
-											)
-										)
-									),
-									Expanded(
-										flex:3,
-										child:editable(
-											beingEdited:false,
-											controller:tempo_controller,
-											numeric:true,
-										)
-									),
-									Expanded(
-										flex:1,
-										child:Align(
-											alignment:Alignment.centerLeft,
-											child:FittedBox(
-												fit:BoxFit.scaleDown,
-												child:Text('Dependencias :')
-											)
-										)
-									),
-									Expanded(
-										flex:10,
-										child:FittedBox(
-											fit:BoxFit.scaleDown,
-											child:Column(
-												children:activities,
-											)
-										)
-									),
-
-								]
+							Expanded(
+								flex:1,
+								child:Align(
+									alignment:Alignment.centerLeft,
+									child:FittedBox(
+										fit:BoxFit.scaleDown,
+										child:Text('Nome :')
+									)
+								)
+							),
+							Expanded(
+								flex:3,
+								child:editable(
+									beingEdited:false,
+									controller:nome_controller,
+								)
+							),
+							Expanded(
+								flex:1,
+								child:Align(
+									alignment:Alignment.centerLeft,
+									child:FittedBox(
+										fit:BoxFit.scaleDown,
+										child:Text('Tempo :')
+									)
+								)
+							),
+							Expanded(
+								flex:3,
+								child:editable(
+									beingEdited:false,
+									controller:tempo_controller,
+									numeric:true,
+								)
+							),
+							Expanded(
+								flex:1,
+								child:Align(
+									alignment:Alignment.centerLeft,
+									child:FittedBox(
+										fit:BoxFit.scaleDown,
+										child:Text('Dependencias :')
+									)
+								)
+							),
+							Expanded(
+								flex:15,
+								child:Container(
+									child:ChipsChoice.multiple(
+										value:marked,
+										options: opts,
+										onChanged: (val) =>print(val),
+									)
+								)
 							)
 						]
 					),
