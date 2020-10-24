8c7e41fac79202c4e7f140c948819a80e2ed9509
varellalexandre <varellalexandre@gmail.com>
2020-10-24 17:36:38 -0300

    Adicionando botão para o site antigo

--- lib/design/field/bar.dart
+++ lib/design/field/bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gspufrn/design/field/color_theme.dart' as theme;
 import 'package:gspufrn/design/field/redirect.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 PreferredSizeWidget NavBar(BuildContext context){
 	return AppBar(
@@ -27,6 +28,21 @@ PreferredSizeWidget NavBar(BuildContext context){
 					path:'/sobre',
 					child:FaIcon(FontAwesomeIcons.users),
 					context:context,
+				)
+			),
+			Tooltip(
+				message:'Software antigo',
+				child:FlatButton(
+					child:FaIcon(FontAwesomeIcons.externalLinkAlt),
+					textColor:Color(0xFFFFFFFF),
+					color:Colors.transparent,
+					onPressed:()async {
+						if(await canLaunch('https://gspufrn.github.io/')){
+							await launch('https://gspufrn.github.io/');
+						}else{
+							throw 'Não foi possível abrir a página';
+						}
+					}
 				)
 			),
 			Container(
